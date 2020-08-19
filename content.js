7989d3aee390f03c6404c685b40d34d5f73ccfd3
danilomacb <danilo.bakun@gmail.com>
2020-08-19 09:11:52 -0300

    images links working

--- content.js
+++ content.js
@@ -1,7 +1,37 @@
-const images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
+let images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
+let tempImage;
 
-Array.prototype.forEach.call(images, (image) => {
-  image.addEventListener("click", () => {
-    window.open(image.childNodes[0].href, "_blank");
+function imagesToLink() {
+  Array.prototype.forEach.call(images, (image) => {
+    tempImage = image;
+    image.removeEventListener("click", openImage);
   });
+
+  images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
+
+  Array.prototype.forEach.call(images, (image) => {
+    tempImage = image;
+    image.addEventListener("click", openImage);
+  });
+}
+
+function openImage() {
+  window.open(tempImage.childNodes[0].href, "_blank");
+}
+
+imagesToLink();
+
+const observer = new MutationObserver((mutationsList) => {
+  for (let i = 0; mutationsList.length > i; i++) {
+    if (mutationsList[i].target.tagName === "DIV") {
+      imagesToLink();
+      break;
+    }
+  }
+});
+
+observer.observe(document.getElementsByClassName("_2z6nI")[0], {
+  attributes: true,
+  childList: true,
+  subtree: true,
 });
