7fb3b209c5d4c78b3f7782bd9c74af1cff5d95c3
danilomacb <danilo.bakun@gmail.com>
2020-08-21 09:38:27 -0300

    waiting 3 secs to run script again, and yes, this is the best i can do now

--- content.js
+++ content.js
@@ -1,53 +1,66 @@
-let images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
-let firstExecution = true;
-let handlers = [];
+function runContent() {
+  let firstExecution = true;
+  let handlers = [];
+  let images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
 
-function imagesToLink() {
-  if (firstExecution) {
+  function imagesLoop() {
     Array.prototype.forEach.call(images, (image) => {
-      const wrappedFunc = openImage.bind(this, image);
+      const wrappedFunc = openImage.bind(image);
       handlers.push(wrappedFunc);
       image.addEventListener("click", wrappedFunc);
     });
-
-    firstExecution = false;
-
-    return;
   }
 
-  removeHandlers();
+  function removeHandlers() {
+    for (var i = 0; images.length > i; i++) {
+      images[i].removeEventListener("click", handlers[i]);
+    }
 
-  images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
+    handlers = [];
+  }
 
-  Array.prototype.forEach.call(images, (image) => {
-    image.addEventListener("click", openImage.bind(this, image));
+  function imagesToLink() {
+    if (firstExecution) {
+      imagesLoop();
+
+      firstExecution = false;
+
+      return;
+    }
+
+    removeHandlers();
+
+    images = document.getElementsByClassName("v1Nh3 kIKUG _bz0w");
+
+    imagesLoop();
+  }
+
+  function openImage() {
+    window.open(this.childNodes[0].href, "_blank");
+  }
+
+  imagesToLink();
+
+  const observer = new MutationObserver((mutationsList) => {
+    for (let i = 0; mutationsList.length > i; i++) {
+      if (mutationsList[i].target.tagName === "DIV") {
+        imagesToLink();
+        break;
+      }
+    }
+  });
+
+  observer.observe(document.getElementsByClassName("_2z6nI")[0], {
+    attributes: true,
+    childList: true,
+    subtree: true,
   });
 }
 
-function openImage(image) {
-  window.open(image.childNodes[0].href, "_blank");
-}
+runContent();
 
-function removeHandlers() {
-  for (var i = 0; images.length > i; i++) {
-    images[i].removeEventListener("click", handlers[i]);
-  }
-  handlers = [];
-}
-
-imagesToLink();
-
-const observer = new MutationObserver((mutationsList) => {
-  for (let i = 0; mutationsList.length > i; i++) {
-    if (mutationsList[i].target.tagName === "DIV") {
-      imagesToLink();
-      break;
-    }
-  }
-});
-
-observer.observe(document.getElementsByClassName("_2z6nI")[0], {
-  attributes: true,
-  childList: true,
-  subtree: true,
+window.addEventListener("change", () => {
+  setTimeout(() => {
+    runContent();
+  }, 3000)
 });
